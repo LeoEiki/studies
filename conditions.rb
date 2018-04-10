0b18a9e4cc6b8a554551525e55e884760f51978a
Global User Name <global_user@email.com>
2018-04-10 15:11:34 -0300

    Adicionando os arquivos conditions.rb e iterations.rb

---
+++ conditions.rb
@@ -0,0 +1,36 @@
+#########################################
+## Preencha todas as ocorrências de __ ##
+#########################################
+
+
+# Operadores condicionais (if/else)
+#
+
+# Atribuição de valores com condições
+if true
+  resultado = 'valor_verdadeiro'
+else
+  resultado = 'valor_falso'
+end
+assert_equal 'valor_verdadeiro', resultado
+
+resultado = 'valor_inicial'
+if true
+  resultado = 'valor_verdadeiro'
+end
+assert_equal 'valor_verdadeiro', resultado
+
+resultado = 'valor_inicial'
+if false
+  resultado = 'valor_falso'
+end
+assert_equal nil, resultado
+
+# Uso de if com uma única linha de código
+resultado = 'valor_inicial'
+resultado = 'valor_verdadeiro' if true
+assert_equal 'valor_verdadeiro', resultado
+
+resultado = 'valor_inicial'
+resultado = 'valor_falso' if false
+assert_equal nil, resultado
